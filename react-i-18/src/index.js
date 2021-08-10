b53f6444804d89af827580980cd8fad1bf1d30d6
muhammadsaloh <muhammadsaloh1706@gmail.com>
2021-08-10 17:24:16 +0500

    Finished

---
+++ react-i-18/src/index.js
@@ -0,0 +1,14 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import App from "./App"
+import "./index.css";
+import "./i18n";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Suspense fallback={<div>Loading...</div>}>
+			<App />
+		</Suspense>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
